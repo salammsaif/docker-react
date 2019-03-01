5fd7f53a19e8fbb835cd9c65dbd65a8a78a29004
saif <salam.msaif@gmail.com>
2019-03-01 23:40:34 +0900

    change text on feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.testtest
+			Changed on feature branch
           </p>
           <a
             className="App-link"
